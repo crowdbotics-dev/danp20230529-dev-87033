4c78c7e59fafab047fa5e65fb2a1152901088f5a
daniel palacios <daniel.palacios@crowdbotics.com>
2023-05-30 09:50:56 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "danp20230529-dev-87033",
-  url: "https://danp20230529-dev-87033.botics.co",
-  api: "https://danp20230529-dev-87033.botics.co/api/v1"
+  "name": "danp20230529-dev-87033",
+  "url": "https://danp20230529-dev-87033.botics.co",
+  "api": "https://danp20230529-dev-87033.botics.co/api/v1",
+  "initialRoute": "ScreenAI2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import Axios2 from "https://cdn.jsdelivr.net/npm/axios@1.3.4/+esm";
+const URL = "https://jsonplaceholder.typicode.com/todos";
 
 const Untitled1 = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    if (!data) {
+      Axios2.get(URL).then(result => {
+        setData(result.data.map(r => r.title));
+      }).catch(err => {
+        setError(err);
+      });
+    }
+  }, []);
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
-    }}></ScrollView>
+    }}>
+        {data && data.join(",")}
+        {error && JSON.stringify(error)}
+      </ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
   }
 });
 export default Untitled1;
